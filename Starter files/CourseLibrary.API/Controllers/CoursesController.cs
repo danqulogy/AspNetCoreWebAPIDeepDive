362843056f2b07f8e7a8baf93e2db8df06a17008
Bernard D. White <danquahwhite@gmail.com>
2024-06-07 17:09:19 +0000

    implemented upsert for put course for author method

--- Starter files/CourseLibrary.API/Controllers/CoursesController.cs
+++ Starter files/CourseLibrary.API/Controllers/CoursesController.cs
@@ -87,7 +87,18 @@ public class CoursesController : ControllerBase
 
         if (courseForAuthorFromRepo == null)
         {
-            return NotFound();
+            var courseToAdd = _mapper.Map<Entities.Course>(course);
+            courseToAdd.Id = courseId;
+            
+            _courseLibraryRepository.AddCourse(authorId,courseToAdd);
+            await _courseLibraryRepository.SaveAsync();
+
+            var courseToReturn = _mapper.Map<CourseDto>(courseToAdd);
+
+            return CreatedAtRoute(
+                "GetCourseForAuthor", 
+                new { authorId, courseId },
+                courseToReturn);
         }
 
         _mapper.Map(course, courseForAuthorFromRepo);
