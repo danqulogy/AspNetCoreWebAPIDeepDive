c44db0a80e4cf34dd0c97ee540480ebc47f80512
Bernard D. White <danquahwhite@gmail.com>
2024-06-07 19:08:35 +0000

    Implemented class level validation

--- Starter files/CourseLibrary.API/Models/CourseForManipulationDto.cs
+++ Starter files/CourseLibrary.API/Models/CourseForManipulationDto.cs
@@ -1,8 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using CourseLibrary.API.ValidationAttributes;
 
 namespace CourseLibrary.API.Models;
 
-public abstract class CourseForManipulationDto: IValidatableObject
+[CourseTitleMustBeDifferentFromDescription]
+public abstract class CourseForManipulationDto //: IValidatableObject
 {
     [Required(ErrorMessage = "You should fill out a tile")]
     [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters")]
@@ -11,14 +13,14 @@ public abstract class CourseForManipulationDto: IValidatableObject
     [MaxLength(1000, ErrorMessage = "The description should not have more than 1500 characters")]
     public virtual string Description { get; set; } = string.Empty;
 
-    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
-    {
-        if (Title == Description)
-        {
-            // Yield return means that the result is returned and after which code execution will continue
-            yield return new ValidationResult(
-                "The provided description should be different from the title.",
-                new[] { "Course" });
-        }
-    }
+    // public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    // {
+    //     if (Title == Description)
+    //     {
+    //         // Yield return means that the result is returned and after which code execution will continue
+    //         yield return new ValidationResult(
+    //             "The provided description should be different from the title.",
+    //             new[] { "Course" });
+    //     }
+    // }
 }
