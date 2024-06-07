7531c228713c8fe6ca4029d89ea942cf7a23900b
Bernard D. White <danquahwhite@gmail.com>
2024-06-07 18:55:15 +0000

    Implemented cross field validation with IValidatable

--- Starter files/CourseLibrary.API/Models/CourseForCreationDto.cs
+++ Starter files/CourseLibrary.API/Models/CourseForCreationDto.cs
@@ -2,13 +2,7 @@
 
 namespace CourseLibrary.API.Models;
 
-public class CourseForCreationDto
+public class CourseForCreationDto:CourseForManipulationDto
 {
-    [Required(ErrorMessage = "You should fill out a tile")]
-    [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters")]
-    public string Title { get; set; } = string.Empty;
-    
-    [MaxLength(1000, ErrorMessage = "The description should have more than 1500 characters")]
-    public string? Description { get; set; } = string.Empty;
 }
 
---
+++ Starter files/CourseLibrary.API/Models/CourseForManipulationDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseLibrary.API.Models;
+
+public abstract class CourseForManipulationDto: IValidatableObject
+{
+    [Required(ErrorMessage = "You should fill out a tile")]
+    [MaxLength(100, ErrorMessage = "The title shouldn't have more than 100 characters")]
+    public string Title { get; set; } = string.Empty;
+    
+    [MaxLength(1000, ErrorMessage = "The description should not have more than 1500 characters")]
+    public virtual string Description { get; set; } = string.Empty;
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (Title == Description)
+        {
+            // Yield return means that the result is returned and after which code execution will continue
+            yield return new ValidationResult(
+                "The provided description should be different from the title.",
+                new[] { "Course" });
+        }
+    }
+}
