21bc907c1bde2ab75d8326ab9affa220e84ed9ab
Bernard D. White <danquahwhite@gmail.com>
2024-06-07 17:13:18 +0000

    implemented upsert for patch  course for author method

--- Starter files/CourseLibrary.API/Controllers/CoursesController.cs
+++ Starter files/CourseLibrary.API/Controllers/CoursesController.cs
@@ -125,7 +125,21 @@ public class CoursesController : ControllerBase
 
         if (courseForAuthorFromRepo == null)
         {
-            return NotFound();
+            var courseDto = new CourseForUpdateDto();
+            patchDocument.ApplyTo(courseDto);
+            
+            var courseToAdd = _mapper.Map<Entities.Course>(courseDto);
+            courseToAdd.Id = courseId;
+            
+            _courseLibraryRepository.AddCourse(authorId,courseToAdd);
+            await _courseLibraryRepository.SaveAsync();
+
+            var courseToReturn = _mapper.Map<CourseDto>(courseToAdd);
+
+            return CreatedAtRoute(
+                "GetCourseForAuthor", 
+                new { authorId, courseId },
+                courseToReturn);
         }
 
         var courseToPatch = _mapper.Map<CourseForUpdateDto>(courseForAuthorFromRepo);
